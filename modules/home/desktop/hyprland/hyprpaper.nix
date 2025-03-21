b68b22fa75ba0493d51e0726e834ee75ed49d2e1
tstokes22 <121993198+tstokes22@users.noreply.github.com>
2025-03-21 19:44:15 -0400

    Update hyprpaper.nix

--- modules/home/desktop/hyprland/hyprpaper.nix
+++ modules/home/desktop/hyprland/hyprpaper.nix
@@ -3,8 +3,14 @@
     enable = true;
     settings = {
       splash = false;
-      preload = [ "~/noise/modules/home/desktop/hyprland/wallpaper/mima-1440.png" ];
-      wallpaper = [ "DP-1,~/noise/modules/home/desktop/hyprland/wallpaper/mima-1440.png" ];
+      preload = [ 
+      "~/dotnix/modules/home/desktop/hyprland/wallpaper/mima-1440.png"
+      "~/dotnix/modules/home/desktop/hyprland/wallpaper/mima-1080.png"
+      ];
+      wallpaper = [ 
+      "DP-1,~/noise/modules/home/desktop/hyprland/wallpaper/mima-1440.png"
+      "HDMI-A-1,~/noise/modules/home/desktop/hyprland/wallpaper/mima-1080.png"
+      ];
     };
   };
 }
